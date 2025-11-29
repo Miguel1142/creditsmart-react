a22b4dbf5727365d1d7b355f9d9bfae34201d828
Miguel1142 <miguel.oyola@est.iudigital.edu.co>
2025-11-29 13:54:29 -0500

    add componentes genericos

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,11 @@
-import React from "react"; 
+import React from "react";
 
-const Footer = () => {
+export const Footer = () => {
     return (
-        <div>
-
-        </div>
+        <footer className="footer">
+            <div className="container"></div>
+            <p>&copy; 2025 CreditSmart - IUDIGITAL</p>
+            <p>Todos los derechos reservados</p>
+        </footer>
     )
 }
-export default Footer;
